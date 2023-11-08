2188d1e5b230ede0cf13bf83d8f3aedc1a42fccf
Julian Lopez <julian.lopez@usa.edu.co>
2023-11-07 19:07:53 -0500

    Actualizacion de mapas

--- Vista/Menu.java
+++ Vista/Menu.java
@@ -1,8 +1,15 @@
 package Dodgeball.Vista;
-
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -12,7 +19,16 @@ import javax.swing.JPanel;
 
 public class Menu extends JFrame {
 
+    private Image backgroundImage; // Variable para almacenar la imagen de fondo
+
     public Menu() {
+        try {
+            backgroundImage = ImageIO.read(new File("Dodgeball/src/Imagenes/Portada.jpg"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         // Configura la ventana
         setTitle("Dodgeball");
         setSize(1500, 900);
@@ -20,7 +36,15 @@ public class Menu extends JFrame {
         setLocationRelativeTo(null);
 
         // Panel principal
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // Dibuja la imagen de fondo en el panel
+                g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+            }
+        };
+        
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setAlignmentX(Component.CENTER_ALIGNMENT); // Centra el panel
 
@@ -28,13 +52,28 @@ public class Menu extends JFrame {
         JLabel titulo = new JLabel("Dodgeball");
         titulo.setFont(new Font("Arial", Font.BOLD, 90));
         titulo.setAlignmentX(Component.CENTER_ALIGNMENT);
+        titulo.setForeground(Color.WHITE);
         panel.add(titulo);
 
         // Botón para iniciar el juego
         JButton botonIniciar = new JButton("Iniciar Juego");
-        JButton botonSalir = new JButton("Salir");
         botonIniciar.setAlignmentX(Component.CENTER_ALIGNMENT);
+        botonIniciar.setPreferredSize(new Dimension(200, 50));
+        botonIniciar.setBackground(new Color(0, 128, 255)); // Color de fondo
+        botonIniciar.setForeground(Color.WHITE); // Color del texto
+        botonIniciar.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Margen interno
+        botonIniciar.setFocusPainted(false); // Quitar el borde de enfoque
+        botonIniciar.setFont(new Font("Arial", Font.BOLD, 50)); // Fuente y tamaño del texto
+
+        // Botón para salir
+        JButton botonSalir = new JButton("Salir");
         botonSalir.setAlignmentX(Component.CENTER_ALIGNMENT);
+        botonSalir.setPreferredSize(new Dimension(200, 50));
+        botonSalir.setBackground(new Color(255, 0, 0));
+        botonSalir.setForeground(Color.WHITE);
+        botonSalir.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        botonSalir.setFocusPainted(false);
+        botonSalir.setFont(new Font("Arial", Font.BOLD, 50));
         botonIniciar.addActionListener(e -> {
 
             Elegir elegir = new Elegir();
@@ -42,7 +81,7 @@ public class Menu extends JFrame {
 
         });
 
-        panel.add(Box.createVerticalStrut(140));
+        panel.add(Box.createVerticalStrut(300));
         panel.add(botonIniciar);
         panel.add(Box.createVerticalStrut(10));
         panel.add(botonSalir);
@@ -52,4 +91,7 @@ public class Menu extends JFrame {
         setVisible(true);
     }
 
+    public static void main(String[] args) {
+        new Menu();
+    }
 }
--- Vista/Tablero.java
+++ Vista/Tablero.java
@@ -3,27 +3,42 @@ package Dodgeball.Vista;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-
-
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Tablero extends JPanel {
 
+    private Image backgroundImage;
+
+    public Tablero() {
+        try {
+            backgroundImage = ImageIO.read(new File("Dodgeball/src/Imagenes/Cancha.jpg")); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
-
         super.paintComponent(g);
 
         int anchoPanel = getWidth();
         int altoPanel = getHeight();
 
-        // Limpiar el panel
+        // Llena el fondo del panel con un color blanco
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, anchoPanel, altoPanel);
 
+        // Dibuja la imagen de fondo
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
         g.setColor(Color.BLACK);
-        int anchoRectangulo = 1200;
-        int altoRectangulo = 700;
+        int anchoRectangulo = 1365;
+        int altoRectangulo = 750;
         int xRectangulo = (anchoPanel - anchoRectangulo) / 2;
         int yRectangulo = (altoPanel - altoRectangulo) / 2;
         g.drawRect(xRectangulo, yRectangulo, anchoRectangulo, altoRectangulo);
@@ -31,8 +46,8 @@ public class Tablero extends JPanel {
         int xLinea = anchoPanel / 2;
         g.drawLine(xLinea, yRectangulo, xLinea, yRectangulo + altoRectangulo);
 
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 25));
 
         String etiquetaIzquierda = "Jugador 1";
         int xEtiquetaIzquierda = (anchoPanel - g.getFontMetrics().stringWidth(etiquetaIzquierda)) / 4;
@@ -44,8 +59,8 @@ public class Tablero extends JPanel {
         int yEtiquetaDerecha = yRectangulo - 20;
         g.drawString(etiquetaDerecha, xEtiquetaDerecha, yEtiquetaDerecha);
 
-        g.setColor(Color.BLACK);
-        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 22));
         String etiquetaCentral = "6 : 6";
         int xEtiquetaCentral = (anchoPanel - g.getFontMetrics().stringWidth(etiquetaCentral)) / 2;
         int yEtiquetaCentral = yEtiquetaIzquierda;
@@ -56,22 +71,24 @@ public class Tablero extends JPanel {
         int altoJugador = 40;
         int espacioEntreJugadores = 10;
         int xInicialIzquierda = xRectangulo + 10;
-        int yInicialIzquierda = yRectangulo + (altoRectangulo - (numJugadores * (altoJugador + espacioEntreJugadores))) / 2;
+        int yInicialIzquierda = yRectangulo
+                + (altoRectangulo - (numJugadores * (altoJugador + espacioEntreJugadores))) / 2;
 
         for (int i = 0; i < numJugadores; i++) {
             int xJugador = xInicialIzquierda;
             int yJugador = yInicialIzquierda + i * (altoJugador + espacioEntreJugadores);
-            g.setColor(Color.RED); 
+            g.setColor(Color.RED);
             g.fillRect(xJugador, yJugador, anchoJugador, altoJugador);
         }
 
         int xInicialDerecha = xRectangulo + anchoRectangulo - anchoJugador - 10;
-        int yInicialDerecha = yRectangulo + (altoRectangulo - (numJugadores * (altoJugador + espacioEntreJugadores))) / 2;
+        int yInicialDerecha = yRectangulo
+                + (altoRectangulo - (numJugadores * (altoJugador + espacioEntreJugadores))) / 2;
 
         for (int i = 0; i < numJugadores; i++) {
             int xJugador = xInicialDerecha;
             int yJugador = yInicialDerecha + i * (altoJugador + espacioEntreJugadores);
-            g.setColor(Color.BLUE); 
+            g.setColor(Color.BLUE);
             g.fillRect(xJugador, yJugador, anchoJugador, altoJugador);
         }
 
