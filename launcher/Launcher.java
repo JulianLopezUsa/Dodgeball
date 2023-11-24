e476b636cf1718b12f8f442276cb237d014147e3
JulianLopezUsa <julian.lopez@usa.edu.co>
2023-11-24 12:24:14 -0500

    Actualización

--- launcher/Launcher.java
+++ launcher/Launcher.java
@@ -5,8 +5,8 @@ import javax.swing.SwingUtilities;
 import Vista.Menu;
 
 public class Launcher {
-    
-  public static void main(String[] args) {
+
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new Menu();
